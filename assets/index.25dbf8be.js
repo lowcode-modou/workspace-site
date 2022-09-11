04fcf07ded7dd13d2f03e0a6b07dbe96a371ac55
ll929 <ll929@users.noreply.github.com>
2022-09-11 21:09:52 +0000

    deploy: lowcode-modou/modou@e2b54db743d52c38dabd4bc9d43885db2a4fad5c

--- assets/index.25dbf8be.js
+++
@@ -1,1 +0,0 @@
-import{u as s,j as e,a as u}from"./index.3916ac14.js";const n=()=>{const a=s();return e("div",{children:[u("div",{children:"\u6211\u662FPage\u9875\u9762"}),u("button",{onClick:()=>a("/apps"),children:"\u8DF3\u8F6C\u5230 APPS \u9875\u9762"})]})};export{n as Page};
