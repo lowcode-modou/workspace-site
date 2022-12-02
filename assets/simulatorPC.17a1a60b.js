7f2415704f21e6d1a363ef32c6ac949e9d80e1c1
ll929 <ll929@users.noreply.github.com>
2022-12-02 19:27:57 +0000

    deploy lowcode-modou/modou@f8d8e8c4d96027e29708f98c18d99b6b5adcea73 73c470fc9dbdf7cd8ecfedac259ff5646a94b8fe

---
+++ assets/simulatorPC.17a1a60b.js
@@ -0,0 +1,1 @@
+import{j as e,bl as t,bm as r,bs as n,br as a,Q as d}from"./client.daed6aac.js";const o=()=>e(t,{backend:r,context:window.parent,children:e(n,{rootWidgetId:"",widgets:[]})});a.createRoot(document.getElementById("simulator_pc_root")).render(e(d.StrictMode,{children:e(o,{})}));
