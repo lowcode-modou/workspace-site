73babffca1841f8b4f2b0768338ad8de80e3a817
ll929 <ll929@users.noreply.github.com>
2022-12-10 08:37:12 +0000

    deploy lowcode-modou/modou@58779cf298311ce6f39a8b65231404760ae2c951 73c470fc9dbdf7cd8ecfedac259ff5646a94b8fe

---
+++ assets/simulatorPC.50f9f700.js
@@ -0,0 +1,1 @@
+import{j as e,bl as t,bm as r,bs as n,br as a,Q as d}from"./client.6a3e2985.js";const o=()=>e(t,{backend:r,context:window.parent,children:e(n,{rootWidgetId:"",widgets:[]})});a.createRoot(document.getElementById("simulator_pc_root")).render(e(d.StrictMode,{children:e(o,{})}));
--- assets/simulatorPC.697f3c9c.js
+++
@@ -1,1 +0,0 @@
-import{j as e,bl as t,bm as r,bs as n,br as a,Q as d}from"./client.a9d773b3.js";const o=()=>e(t,{backend:r,context:window.parent,children:e(n,{rootWidgetId:"",widgets:[]})});a.createRoot(document.getElementById("simulator_pc_root")).render(e(d.StrictMode,{children:e(o,{})}));
